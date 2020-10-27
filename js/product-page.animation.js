28875b0f9df6c39271803356a713682611d28512
Tran Thanh Khang <360tranthanhkhang2281999@gmail.com>
2020-10-27 17:50:09 +0700

    complete sidebar of product page

---
+++ js/product-page.animation.js
@@ -0,0 +1,38 @@
+const viewModeGrid = document.querySelector('.view-mode--grid');
+const viewModeList = document.querySelector('.view-mode--list');
+
+let filterPrice = { id: 'filter-price', isAngleUp: true };
+let filterBrand = { id: 'filter-brand', isAngleUp: true };
+
+// Toggle choosing display mode of list of products;
+viewModeGrid.addEventListener('click', (e) => {
+	e.target.classList.add('active');
+	viewModeList.classList.remove('active');
+});
+
+viewModeList.addEventListener('click', (e) => {
+	e.target.classList.add('active');
+	viewModeGrid.classList.remove('active');
+});
+
+// Rotate Z 180 deg to angle up of filter title
+const rotateAngle = (filter, angle) => {
+	if (filter.isAngleUp) {
+		angle.style.transform = 'rotateZ(180deg)';
+	} else {
+		angle.style.transform = 'none';
+	}
+
+	filter.isAngleUp = !filter.isAngleUp;
+};
+
+const rotateZAngleUp = (element) => {
+	const id = element.id;
+	const angle = element.children[0].children[0];
+
+	if (id === filterPrice.id) {
+		rotateAngle(filterPrice, angle);
+	} else {
+		rotateAngle(filterBrand, angle);
+	}
+};
