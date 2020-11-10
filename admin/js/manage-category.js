85bbeceef8008ef6df5597967adaeb2df63a8d39
quanghuy74 <huy25107@gmail.com>
2020-11-10 12:36:50 +0700

    fix ui page manage-category

--- admin/js/manage-category.js
+++ admin/js/manage-category.js
@@ -1,129 +1,142 @@
 var category = [
-    {
-        index: 0,
-        name: "Son Xáp",
-        description: "123",
-        image: "https://media3.scdn.vn/img3/2019/3_17/OWbf3h_simg_de2fe0_500x500_maxb.jpg"
-    },
-    {
-        index: 1,
-        name: "Son Nước",
-        description: "123",
-        image: "https://media3.scdn.vn/img3/2019/7_3/ubH8on_simg_de2fe0_500x500_maxb.jpg"
-    },
-    {
-        index: 2,
-        name: "Son Dưỡng",
-        description: "123",
-        image: "https://product.hstatic.net/1000025647/product/son-duong-dior-001-pink_grande_9e54348245de42f5a816f49a3bbc30bc_grande.png"
-    }
-]
-
-checkData();
-showCategory();
+  {
+    index: "DM0",
+    name: "Son Xáp",
+    description: "123",
+    image:
+      "https://media3.scdn.vn/img3/2019/3_17/OWbf3h_simg_de2fe0_500x500_maxb.jpg",
+  },
+  {
+    index: "DM1",
+    name: "Son Nước",
+    description: "123",
+    image:
+      "https://media3.scdn.vn/img3/2019/7_3/ubH8on_simg_de2fe0_500x500_maxb.jpg",
+  },
+  {
+    index: "DM2",
+    name: "Son Dưỡng",
+    description: "123",
+    image:
+      "https://product.hstatic.net/1000025647/product/son-duong-dior-001-pink_grande_9e54348245de42f5a816f49a3bbc30bc_grande.png",
+  },
+];
 
 function showCategory() {
-    document.querySelector(".table-category").innerHTML = ""
+  document.querySelector(".table-category").innerHTML = "";
+  category.map((item) => {
+    var row = `<tr>
+            <td> ${item.index} </td>
+            <td> ${item.name} </td>
+            <td> ${item.description} </td>
+            <td><img class="img-thumbnail" src="${item.image}"/></td>
+            <td><input class="btn btn-success" type="button" value="Sửa" onclick="editCategory('${item.index}')"></td>
+            <td><input class="btn btn-danger" type="button" value="Xóa" onclick="deleteCategory('${item.index}')"></td>
+        </tr>`;
 
-    category = getCategory()
-    category.map(item => { 
-        var row = '<tr>' + 
-            '<td>' +item.index+ '</td>'+
-            '<td>' +item.name+ '</td>'+
-            '<td>' +item.description+ '</td>'+
-            '<td><img class="img-thumbnail" src="'+item.image+'"/></td>'+
-            '<td><input class="btn btn-outline-dark" type="button" value="Chỉnh sửa" onclick="editCategory(' + item.index + ')"></td>'+
-            '<td><input class="btn btn-outline-dark" type="button" value="Xóa" onclick="deleteCategory(' + item.index + ')"></td>'+
-        '</tr>'
-        
-        document.querySelector(".table-category").innerHTML += row;
-    })
+    document.querySelector(".table-category").innerHTML += row;
+  });
 }
 
 function save() {
-    var index = document.getElementById("index").value;
-    var name = document.getElementById("name").value;
-    var description = document.getElementById("description").value;
-    var image = document.getElementById("image").value;
-    category = getCategory();
-    var item
+  getCategoryFromStorage()
 
-    if(index == "") {
-        item = {
-            index: category.length,
-            name: name,
-            description: description,
-            image: image
-        };
-    }else {
-        item = {
-            index: index.substr(index.length - 1),
-            name: name,
-            description: description,
-            image: image
-        };
-    }
-    updateCategory(item);
-    showCategory();
-    showForm();
+  var index = document.getElementById("index").value;
+  var name = document.getElementById("name").value;
+  var description = document.getElementById("description").value;
+  var image = document.getElementById("image").value;
+
+  var item = {
+    index: index,
+    name: name,
+    description: description,
+    image: image,
+  };
+  if (index == "") {
+    index = category[category.length - 1].index;
+    index = parseInt(index.slice(2, index.length));
+    index++;
+    index = "DM" + index;
+    item.index = index;
+    category.push(item);
+  } else {
+    category = category.map(cate => cate.index === item.index ?   item : cate);
+  }
+
+
+  saveCategoryToStorage();
+  showCategory();
+  showForm();
 }
 function editCategory(index) {
-    showForm();
-    category = getCategory();
-    document.getElementById("index").value = index
-    document.getElementById("name").value = category[index].name
-    document.getElementById("description").value = category[index].description
-    document.getElementById("image").value = category[index].image
+  showForm();
+  getCategoryFromStorage();
+  index = index.slice(2, index.length);
+  document.getElementById("index").value = "DM" + index;
+  document.getElementById("name").value = category[index].name;
+  document.getElementById("description").value = category[index].description;
+  document.getElementById("image").value = category[index].image;
 }
 function deleteCategory(index) {
-    category = getCategory();
-
-    category.splice(index,1);
-    localStorage.setItem("category", JSON.stringify(category))
-
-    showCategory()
+  getCategoryFromStorage();
+  category = category.filter((item) => item.index != index);
+  saveCategoryToStorage();
+  showCategory();
 }
 
-function showForm(){
-    category = getCategory();
-    document.getElementById("index").value = "";
-    document.getElementById("name").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("image").value = "";
-    var display = document.querySelector(".form-category").style.display;
+function showForm() {
+  getCategoryFromStorage();
+  document.getElementById("index").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("image").value = "";
+  var display = document.querySelector(".form-category").style.display;
 
-    if (display === "block") {
-        document.querySelector(".form-category").style.display = "none";
-        document.querySelector(".table").style.display = "table";
-
-    }else {
-        document.querySelector(".form-category").style.display = "block";
-        document.querySelector(".table").style.display = "none";
-
-    }
+  if (display === "block") {
+    document.querySelector(".form-category").style.display = "none";
+    document.querySelector(".table").style.display = "table";
+  } else {
+    document.querySelector(".form-category").style.display = "block";
+    document.querySelector(".table").style.display = "none";
+  }
 }
 
-function updateCategory(item) {
-    category = getCategory()
 
-    if(item.index < category.length){
-        category[item.index] = item;
-    }else {
-        category.push(item);
-    }
-    localStorage.setItem("category", JSON.stringify(category));
+function searching(inputText) {
+  inputText = inputText.toLowerCase().trim();
+  inputText = inputText.replace(/\s\s+/g, " ");
+
+  getCategoryFromStorage();
+
+  if (inputText == "") {
+    showCategory();
+    return;
+  }
+
+  let keywords = inputText.split(" ");
+  category = category.filter((eachUser) => {
+    return Object.keys(eachUser).some((key) => {
+      return keywords.reduce(
+        (acc, word) => acc && eachUser[key].toLowerCase().includes(word),
+        true
+      );
+    });
+  });
+  showCategory();
 }
 
-function getCategory(){
-    var data = localStorage.getItem("category");
-    data = JSON.parse(data);
-    return data;
+function getCategoryFromStorage() {
+  let categoryString = localStorage.getItem("category");
+  category = JSON.parse(categoryString) || [];
 }
 
-function checkData(){
-    if(!category){
-        category = getCategory();
-    }else{
-        localStorage.setItem("category", JSON.stringify(category));
-    }
+function saveCategoryToStorage() {
+  let categoryString = JSON.stringify(category);
+  localStorage.setItem("category", categoryString);
 }
+
+function loadData() {
+  getCategoryFromStorage();
+  showCategory();
+}
+
