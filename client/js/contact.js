99710fd83203253e71008dd022a94ef901c0940d
quanghuy74 <huy25107@gmail.com>
2020-11-10 17:24:05 +0700

    add form contact

---
+++ client/js/contact.js
@@ -0,0 +1,39 @@
+var contact = [];
+
+function showForm() {
+  var display = document.querySelector(".form-contact").style.display;
+  if(display === "block"){
+    document.querySelector(".form-contact").style.display = "none";
+  }else{
+    document.querySelector(".form-contact").style.display = "block";
+  }
+}
+
+function sent() {
+  getContactFromStorage();
+
+  var name = document.querySelector("#name").value;
+  var email = document.querySelector("#email").value;
+  var message = document.querySelector("#message").value;
+
+  var newContact = {
+    id: "CT" + contact.length,
+    name: name,
+    email:email,
+    message:message
+  }
+
+  contact.push(newContact);
+  saveContactToStorage();
+  showForm();
+}
+
+function getContactFromStorage(){
+  let contactString = localStorage.getItem("contact");
+  contact = JSON.parse(contactString) || [];
+}
+
+function saveContactToStorage() {
+  let contactString = JSON.stringify(contact);
+  localStorage.setItem("contact", contactString);
+}
