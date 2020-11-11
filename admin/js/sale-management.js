d2ab153172724b162ed842db2f8b32b9b29d889d
Van Ba Loc <vbloc99@gmail.com>
2020-11-11 18:33:11 +0700

    solved conflict 11/11

--- admin/js/sale-management.js
+++ admin/js/sale-management.js
@@ -1,119 +1,205 @@
-var gCategories = [
-  {
-    maDM: "DM001",
-    tenDM: "Son môi",
-    mota: "Son tô môi cho phụ nữ",
-    anh:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRMBSJqT3LWBxOdcd6AMAnbkDmDfDib55q6yw&usqp=CAU",
-  },
-  {
-    maDM: "DM002",
-    tenDM: "Phấn",
-    mota: "Phấn má cho phụ nữ",
-    anh:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRMBSJqT3LWBxOdcd6AMAnbkDmDfDib55q6yw&usqp=CAU",
-  },
-  {
-    maDM: "DM003",
-    tenDM: "Nước hoa",
-    mota: "Nước hoa cho phụ nữ",
-    anh:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRMBSJqT3LWBxOdcd6AMAnbkDmDfDib55q6yw&usqp=CAU",
-  },
-];
+var gCategories = gCategories || [];
+var gProducts = gCategories || [];
+var gSales = gCategories || [];
+var detailSale = null;
+class Sale {
+  id = "";
+  salename = "";
+  description = "";
+  discount = "";
+  beginDate = "";
+  endDate = "";
+  products = [];
+  constructor() {
+    let len = gSales.length;
+    if (len > 0) {
+      let id = gSales[gSales.length - 1].id;
+      id = "KM" + (parseInt(id.slice(2, id.length)) + 1);
+      this.id = id;
+    } else {
+      this.id = "KM1";
+    }
+  }
+}
 
-var gSales = [];
-
-function loadSaleTable() {
+function loadSaleTable(sales) {
   let tblBody = document.querySelector("#tblBody");
-  let no = 0;
   tblBody.innerHTML = "";
 
-  gSales.forEach((sale) => {
-    tblBody.innerHTML += `
+  sales.forEach((sale) => {
+    tblBody.innerHTML += getRowBySale(sale);
+  });
+}
+
+function getRowBySale(sale) {
+  let row = `
     <tr>
       <td>${sale.id}</td>
       <td>${sale.salename}</td>
-      <td>${sale.discount}</td>
+      <td>${sale.discount}%</td>
       <td>${sale.beginDate}</td>
       <td>${sale.endDate}</td>
       <td>${sale.description}</td>
-      <td>${getCategoryName(sale)}</td>
       <td>
-        <button class="btn btn-success" onclick="editSale(${
-          sale.id
-        })">Sửa</button>
-        <button class="btn btn-danger" onclick="deleteSale(${
-          sale.id
-        })">Xoá</button>
+        <button class="btn btn-success" onclick="showForm('${sale.id}')">Sửa</button>
+        <button class="btn btn-danger" onclick="deleteSale('${sale.id}')">Xoá</button>
       </td>
    </tr>
     `;
-  });
+  return row;
 }
 
-// function getCate
+function loadCategoryTab() {
+  let tab = document.querySelector("#cateTab");
+  let content = document.querySelector("#contentTab");
 
-function loadFormCategories() {
-  let e = document.querySelector("#cateSelecting");
+  tab.innerHTML = "";
+  content.innerHTML = "";
   gCategories.forEach((cate) => {
-    e.innerHTML += `
-    <div class="form-check form-check-inline col-md-2">
+    tab.innerHTML += getTabButtonByCate(cate);
+    content.innerHTML += createContentTabByCate(cate);
+  });
+
+  document.getElementById("tabDM1").click();
+}
+
+function createContentTabByCate(cate) {
+  let contentBox = "";
+  contentBox += `
+    <div id=${cate.maDM} class="tabcontent col-md-10">
+      <div class="form-check form-check-inline col-md-5">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="selectAll${cate.maDM}"
+          value="${cate.maDM}"
+          onClick="clickSelectAll(this)"
+        />
+        
+        <label class="form-check-label" for="selectAll${cate.maDM}"
+          >Chọn tất cả</label
+        >
+      </div>
       <input
-        class="form-check-input"
-        type="checkbox"
-        id="${cate.maDM}"
-        value="${cate.maDM}"
-        name="category"
+        type="text"
+        class="col-md-5"
+        id="productSearch"
+        name="productSearch"
+        placeholder="Tìm kiếm sản phẩm"
       />
-      <label class="form-check-label" for="${cate.maDM}"
-        >${cate.tenDM}</label
-      >
+      ${createCheckboxListByCate(cate)}
     </div>
+  `;
+  return contentBox;
+}
+
+function createCheckboxListByCate(cate) {
+  let products = gProducts.filter((product) => product.maDM === cate.maDM);
+  let checkedProducts = detailSale.products;
+
+  let checkboxes = "";
+  products.forEach((product) => {
+    let check;
+    if (checkedProducts.includes(product.maSP)) {
+      check = "checked='checked'";
+    }
+    checkboxes += `
+      <div class="form-check form-check-inline col-md-5">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="${product.maSP}"
+          value="${product.maDM}"
+          name="product"
+          ${check}
+        />
+      
+        <label class="form-check-label" for="${product.maSP}"
+          >${product.tenSP}</label
+        >
+      </div>
     `;
   });
+  return checkboxes;
 }
 
-function clickAdd() {
-  let id = gSales.length;
+function getTabButtonByCate(cate) {
+  let tabButton = `
+    <button 
+      type="button"
+      class="tablinks"
+      onclick="openCate(event, '${cate.maDM}')"
+      id=tab${cate.maDM}
+    >
+      ${cate.tenDM}
+    </button>
+  `;
+  return tabButton;
+}
+
+function clickSelectAll(checkbox) {
+  // Get all of product checkboxs
+  let pChecks = document.querySelectorAll('[name="product"]');
+
+  let cate = checkbox.value;
+  pChecks.forEach((pCheck) => {
+    if (pCheck.value == cate) pCheck.checked = checkbox.checked;
+  });
+}
+
+/**
+ * @return {Array<string>} List of id of checked products.
+ */
+function getSelectedProducts() {
+  let products = document.querySelectorAll('[name="product"]:checked');
+  let ids = [...products].map((product) => product.id);
+  return ids;
+}
+
+function clickFinish() {
   let salename = frm.salename.value;
   let description = frm.description.value;
   let discount = frm.discount.value;
   let beginDate = frm.beginDate.value;
   let endDate = frm.endDate.value;
 
-  let newSale = {
-    id: id,
+  detailSale = {
+    id: detailSale.id,
     salename: salename,
     description: description,
     discount: discount,
     beginDate: beginDate,
     endDate: endDate,
-    categories: getSelectedCategories(),
+    products: getSelectedProducts(),
   };
-  gSales.push(newSale);
+
+  let isExist = gSales.findIndex((sale) => sale.id == detailSale.id);
+  if (isExist === -1) {
+    gSales.push(detailSale);
+    alert("Thêm dữ liệu thành công!");
+  } else {
+    gSales[isExist] = detailSale;
+    alert("Sửa đổi dữ liệu thành công!");
+  }
   saveSalesToStorage();
-  loadSaleTable();
-  console.log(newSale);
+  loadSaleTable(gSales);
+  toggleForm();
 }
 
-function getCategoryName(sale) {
-  let ids = sale.categories;
-  let cateNames = gCategories
-    .filter((cate) => ids.includes(cate.maDM))
-    .map((cate) => cate.tenDM);
-  return cateNames;
-}
-
-function getSelectedCategories() {
-  let categories = document.querySelectorAll('[name="category"]:checked');
-  // console.log(categories);
-  let ids = [...categories].map((cate) => cate.value);
-  return ids;
+/**
+ * @typedef {Object} sale
+ * @return {Array<String>}
+ */
+function getProductsName(sale) {
+  let ids = sale.products;
+  let productsName = gProducts
+    .filter((product) => ids.includes(product.maSP))
+    .map((product) => product.tenSP);
+  return productsName;
 }
 
 function clickCancel() {
-  alert("This is cancel");
+  toggleForm();
 }
 
 function editSale(id) {
@@ -123,7 +209,7 @@ function editSale(id) {
 function deleteSale(id) {
   gSales = gSales.filter((sale) => sale.id != id);
   saveSalesToStorage();
-  loadSaleTable();
+  loadSaleTable(gSales);
 }
 
 function getSalesFromStorage() {
@@ -136,9 +222,95 @@ function saveSalesToStorage() {
   localStorage.setItem("sales", salesString);
 }
 
-function loadData() {
+async function loadData() {
   getSalesFromStorage();
+  let data = await fetchData("../../data/categories.json");
+  gCategories = data.categories || [];
+  data = await fetchData("../../data/products.json");
+  gProducts = data.products || [];
 
-  loadSaleTable();
-  loadFormCategories();
+  loadSaleTable(gSales);
+}
+
+function fetchData(link) {
+  let data = fetch(link)
+    .then((response) => response.json())
+    .then((data) => data);
+  return data;
+}
+
+function openCate(evt, category) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(category).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function showForm(id) {
+  toggleForm();
+  let btnAdd = document.getElementById("btnAdd");
+  let article = document.getElementsByClassName("title-article")[0];
+  detailSale = getSaleByID(id) || new Sale();
+
+  setFormField();
+  loadCategoryTab();
+
+  if (id == null) {
+    btnAdd.textContent = "Thêm";
+    article.textContent = "TẠO MỚI KHUYẾN MÃI";
+  } else {
+    btnAdd.textContent = "Cập nhật";
+    article.textContent = "CẬP NHẬT KHUYẾN MÃI";
+  }
+}
+
+function setFormField() {
+  frm.salename.value = detailSale.salename;
+  frm.description.value = detailSale.description;
+  frm.discount.value = detailSale.discount;
+  frm.beginDate.value = detailSale.beginDate;
+  frm.endDate.value = detailSale.endDate;
+}
+
+function getSaleByID(id) {
+  let sale = gSales.find((sale) => sale.id == id);
+  return sale;
+}
+
+function toggleForm() {
+  console.log("Hello");
+  let frm = document.querySelector(".input-frm");
+  let sales = document.querySelector(".show-sales");
+  console.log(sales);
+  frm.classList.toggle("d-none");
+  sales.classList.toggle("d-none");
+}
+
+function searching(inputText) {
+  inputText = inputText.toLowerCase().trim();
+  inputText = inputText.replace(/\s\s+/g, " ");
+  console.log(inputText);
+  if (inputText == "") {
+    loadSaleTable(gSales);
+    return;
+  }
+
+  let keywords = inputText.split(" ");
+  let sSales = gSales.filter((eachSale) => {
+    return Object.keys(eachSale).some((key) => {
+      return keywords.reduce(
+        (acc, word) =>
+          acc && eachSale[key].toString().toLowerCase().includes(word),
+        true
+      );
+    });
+  });
+  loadSaleTable(sSales);
 }
--- client/js/generate.js
+++ client/js/generate.js
@@ -33,9 +33,9 @@ for (let i = 1; i <= 200; i++) {
     ngaySanXuat: 'Xem trên bao bì sản phẩm.',
     thuongHieu: thuongHieu,
     moTa:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam laboriosam fugiat eum nihil deserunt odio velit iure repellendus hic suscipit eius voluptas repudiandae enim ex consectetur assumenda commodi, nisi alias.',
-    anh: '../../img/Chambor_Rouge_Plum+_Lipstick.webp',
-    ngayNhap: ngayNhap
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam laboriosam fugiat eum nihil deserunt odio velit iure repellendus hic suscipit eius voluptas repudiandae enim ex consectetur assumenda commodi, nisi alias.",
+    ngayNhap: ngayNhap,
+    anh: "../../img/product-img/product1.webp",
   };
 
   products.push(product);
