116eeaeb995a30cad792ce3ffe659e81080acc30
SonUED <45086369+SonUED@users.noreply.github.com>
2020-12-05 22:17:15 +0700

    init firebase

---
+++ client/js/firebaseExample.js
@@ -0,0 +1,16 @@
+var database = firebase.database();
+// products data : products
+// detailOrder data : detailOrder
+// categories data : categories
+// Insert example
+const insert = (product) => {
+  // database
+  //   .ref("products")
+  //   .push()
+  //   .set({
+  //     ...product,
+  //   })
+  //   .then(() => {
+  //     console.log("Done");
+  //   });
+};
---
+++ util/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADyGkEwN6yjy1UJto8q7-4VBBt0N-HN5A",
+  authDomain: "hf-beauty.firebaseapp.com",
+  databaseURL: "https://hf-beauty-default-rtdb.firebaseio.com",
+  projectId: "hf-beauty",
+  storageBucket: "hf-beauty.appspot.com",
+  messagingSenderId: "901776832063",
+  appId: "1:901776832063:web:f87ef1eb90a1d92ac8ea8a",
+  measurementId: "G-Y954HNRFY5",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
